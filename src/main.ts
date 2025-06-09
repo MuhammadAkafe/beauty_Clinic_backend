6d105b2361c7cfcac6cc77ce1ceada4f8f28c6af
muhammad aqfa <allappsdata97@gmail.com>
2025-06-09 03:15:36 +0300

    fc

--- src/main.ts
+++ src/main.ts
@@ -12,12 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(cors(
-    {
-        origin: "http://localhost:2000",
-        credentials: true,
-    }
-));
+app.use(cors({
+    origin: 'http://localhost:2000',
+    credentials: true,
+  }));
 
 
 app.use(cookieParser());
