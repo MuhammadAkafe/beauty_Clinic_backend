64e54aafaa9342b5543c70f4295e7a7ef11007d6
muhammad aqfa <allappsdata97@gmail.com>
2025-06-11 18:09:06 +0300

    Refactor database connection settings to use DATABASE_URL environment variable in data source configuration

--- src/data-source/data-source.ts
+++ src/data-source/data-source.ts
@@ -6,11 +6,7 @@ import path from "path";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "root",
-    password: "1234",
-    database: "beautyclinic",
+    url: process.env.DATABASE_URL,
     synchronize: false,
     logging: true,
     migrations: [path.join(__dirname, "../migrations/*.ts")],
